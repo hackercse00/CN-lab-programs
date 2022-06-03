06f87c44bc69e5dbbf5e7775a0f3b5f361f2f7f1
SasiVardhan247 <83742554+SasiVardhan247@users.noreply.github.com>
2022-06-03 10:13:02 +0530

    Update Hammingcode.c
    Hamming code is technique developed by R.W. Hamming for error correction. This method corrects the error by finding the state at which the error has occurred.

--- Hammingcode.c
+++ Hammingcode.c
@@ -5,7 +5,7 @@ Created on:02-06-2022 10:07A.M
 #include<math.h>
 void sender();
 void reciever();
-int i,n,j,r=1,x,t=0,count=0;
+int i,n,j,r=1,x,t=0,count=0,pos;
 void main(){
         char s;
         printf("Choose S to send the data and R to recieve:\n");
@@ -18,10 +18,9 @@ void main(){
 void sender(){
         printf("Enter data set size:\n");
         scanf("%d",&n);
-        int a[n+1];
         while(pow(2,r)-r<n+1) r++;
         printf("Redundant bits are %d\n",r);
-        int b[n+r+1],d[r],c[n+r+1][r+1];
+        int a[n+1],b[n+r+1],d[r],c[n+r+1][r+1];
         printf("Enter senders data\n");
         for(i=1;i<=n;i++)
                 scanf("%d",&a[i]);
@@ -44,7 +43,7 @@ void sender(){
         count=0;
         for(i=1;i<=n+r;i++){
                 if(c[i][j]==1){
-                      if(b[i]==1) count++;
+                        if(b[i]==1) count++;
                 }
         }
         if(count%2==0) b[d[j]]=0;
@@ -57,13 +56,38 @@ void sender(){
 void reciever(){
         printf("Enter data set size:\n");
         scanf("%d",&n);
-        int b[n];
         while(n>pow(2,r)) r++;
         printf("Redundant bits are %d\n",r);
-        int a[n-r];
+        int a[(n-r)+1],d[r+1],b[n+1],c[n+1][r+1];
         printf("Enter Code Word:\n");
-        for(i=1;i<=n;i++){
+        for(i=1;i<=n;i++)
                 scanf("%d",&b[i]);
+         for(i=1;i<=n;i++){
+                x=i;
+                for(j=1;j<=r;j++){
+                        c[i][j]=x%2;
+                        x/=2;
+                }
+        }
+        for(j=1;j<=r;j++){
+        count=0;
+        for(i=1;i<=n;i++){
+                if(c[i][j]==1){
+                        if(b[i]==1) count++;
+                }
+        }
+        if(count%2==0) d[j]=0;
+        else d[j]=1;
+        pos+=d[j]*pow(2,j-1);
+        }
+        if(pos==0) printf("Error Free\n");
+        else{
+                printf("Error at position %d\n",pos);
+                if(b[pos]==1) b[pos]=0;
+                else b[pos]=1;
+        }
+        t=0;
+        for(i=1;i<=n;i++){
                 if(i==pow(2,t)){
                         t++;
                         continue;
@@ -73,4 +97,4 @@ void reciever(){
         printf("Recievers data is\n");
         for(i=1;i<=n-r;i++)
                 printf("%d ",a[i]);
-}                                                               
+}
