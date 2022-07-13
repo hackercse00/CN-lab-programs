cf34a08ff66b39a32b451157987e8f085138aaa9
hacker <104961225+hackercse00@users.noreply.github.com>
2022-07-14 01:34:26 +0530

    Update pri.c

--- pri.c
+++ pri.c
@@ -1,36 +1,35 @@
 #include<stdio.h>
 #include<limits.h>
-int i,j,u,v,mincost,count,min ;
+int i,j,u,v,x,count,min,ans;
 void main(){
 	int n;
 	scanf("%d",&n);
-	int visited[n];
-	int cost[n][n];
+	int visited[n],cost[n][n];
 	for(i=0;i<n;i++){
 		for(j=0;j<n;j++){
 			scanf("%d",&cost[i][j]);
-			if(cost[i][j]==0) cost[i][j]=999;
+			if(cost[i][j]==0) cost[i][j]=INT_MAX;
 		}
 		visited[i]=0;
 	}
-	visited[0]=1;
+	scanf("%d",&x);
+	visited[x]=1;
 	for(count=0;count<n-1;count++){
-		for(i=0,min=999;i<n;i++)
+	for(i=0,min=INT_MAX;i<n;i++){
 		for(j=0;j<n;j++){
-			if(cost[i][j]<min){
-				if(visited[i]!=0){
-					min=cost[i][j];
-					u=i;
-					v=j;
-				}	
-			} 
+			if(cost[i][j]<min&visited[i]!=0){
+				min=cost[i][j];
+				u=i;
+				v=j;
+			}
 		}
-		if(visited[v]==0){ 
-			 printf("%d to %d cost= %d\n",u,v,min);
-			 mincost+=min;
-		visited[v]=1;
-		}
-		cost[u][v]=cost[v][u]=999;
 	}
-	printf("%d",mincost);
+	if(visited[v]==0){
+		visited[v]=1;
+		printf("%d to %d min is %d\n",u,v,min);
+		ans+=min;
+	}
+	cost[u][v]=cost[v][u]=INT_MAX;
+}
+printf("%d",ans);
 }
